4ed20927891d4666e0b9b39550907d84f28d648d
Patrick Fernie <patrick.fernie@gmail.com>
2022-08-29 23:37:48 -0400

    style: fix release.sh comments/whitespace

--- release.sh
+++ release.sh
@@ -10,12 +10,12 @@ fi
 
 # takes the tag as an argument (e.g. v0.1.0)
 if [ -n "$1" ]; then
-	# update the version
-	msg="# managed by release.sh"
-	sed "s/^version = .* $msg$/version = \"${1#v}\" $msg/" -i Cargo.toml
-	# update the changelog
-	git cliff --date-order --sort newest --unreleased --tag "$1" --prepend CHANGELOG.md
-	git add -A
+  # update the version
+  msg="# managed by release.sh"
+  sed "s/^version = .* $msg$/version = \"${1#v}\" $msg/" -i Cargo.toml
+  # update the changelog
+  git cliff --date-order --sort newest --unreleased --tag "$1" --prepend CHANGELOG.md
+  git add -A
   git diff --cached
   echo -e -n "\e[33mProceed? \e[0m"
   read -n 1 -s -p "[y/N] " proceed
@@ -26,20 +26,18 @@ if [ -n "$1" ]; then
     exit
   fi
   git commit -m "chore(release): prepare for $1"
-	git show
-	# generate a changelog for the tag message
-	export GIT_CLIFF_TEMPLATE="\
-	{% for group, commits in commits | group_by(attribute=\"group\") %}
-	{{ group | upper_first }}\
-	{% for commit in commits %}
-		- {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-	{% endfor %}
-	{% endfor %}"
-	changelog=$(git cliff --date-order --sort newest --unreleased --strip all)
-	# create a signed tag
-	# https://keyserver.ubuntu.com/pks/lookup?search=0x4A92FA17B6619297&op=vindex
-	git tag -a "$1" -m "Release $1" -m "$changelog"
-  git show -q "$1"
-else
-	echo "warn: please provide a tag"
-fi
+  git show
+  # generate a changelog for the tag message
+  export GIT_CLIFF_TEMPLATE="\
+    {% for group, commits in commits | group_by(attribute=\"group\") %}
+    {{ group | upper_first }}\
+      {% for commit in commits %}
+      - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
+        {% endfor %}
+        {% endfor %}"
+          changelog=$(git cliff --date-order --sort newest --unreleased --strip all)
+          git tag -a "$1" -m "Release $1" -m "$changelog"
+          git show -q "$1"
+        else
+          echo "warn: please provide a tag"
+      fi
