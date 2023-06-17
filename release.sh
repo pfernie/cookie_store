0cb8abf9075af06a1d785cdebf380593a2ca09a0
Patrick Fernie <patrick.fernie@gmail.com>
2023-06-17 10:56:46 -0400

    ci: allow specification of last tag to generate CHANGELOG from

--- release.sh
+++ release.sh
@@ -14,11 +14,15 @@ if [ -n "$1" ]; then
 		echo -e "\e[31m${1} not a version of the expected format; please use v#.#.# format\e[0m"
 		exit
 	fi
+	since_flag="--unreleased"
+	if [[ ! -z "${2}" ]]; then
+		since_flag="${2}"
+	fi
 	# update the version
 	msg="# managed by release.sh"
 	sed "s/^version = .* $msg$/version = \"${1#v}\" $msg/" -i Cargo.toml
 	# update the changelog
-	git cliff --topo-order --sort newest --unreleased --tag "$1" --prepend CHANGELOG.md
+	git cliff --topo-order --sort newest $since_flag --tag "$1" --prepend CHANGELOG.md
 	git diff
 	echo -e -n "\e[33mProceed? \e[0m"
 	read -n 1 -s -p "[y/N] " proceed
@@ -39,7 +43,7 @@ if [ -n "$1" ]; then
       - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
         {% endfor %}
         {% endfor %}"
-	changelog=$(git cliff --topo-order --sort newest --unreleased --strip all)
+	changelog=$(git cliff --topo-order --sort newest $since_flag --strip all)
 	git tag "$1" -m "Release $1" -m "$changelog"
 	git show -q "$1"
 else
