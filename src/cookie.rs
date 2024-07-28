3990557f16e86933e2e916b7c6f4e7077a9a290e
David Campbell <dcampbell24@gmail.com>
2024-07-28 17:49:23 -0400

    Make cookie cookies.

--- src/cookie.rs
+++ src/cookie.rs
@@ -272,29 +272,29 @@ pub mod cookie_store_serialized {
 
     /// Load cookies from `reader`, deserializing with `cookie_from_str`, skipping any __expired__
     /// cookies
-    pub fn load<R, E, F>(reader: R, cookie_from_str: F) -> StoreResult<CookieStore>
+    pub fn load<R, E, F>(reader: R, cookies_from_str: F) -> StoreResult<CookieStore>
     where
         R: BufRead,
         F: Fn(&str) -> Result<CookieStoreSerialized<'static>, E>,
         crate::Error: From<E>,
     {
-        load_from(reader, cookie_from_str, false)
+        load_from(reader, cookies_from_str, false)
     }
 
     /// Load cookies from `reader`, deserializing with `cookie_from_str`, loading both __unexpired__
     /// and __expired__ cookies
-    pub fn load_all<R, E, F>(reader: R, cookie_from_str: F) -> StoreResult<CookieStore>
+    pub fn load_all<R, E, F>(reader: R, cookies_from_str: F) -> StoreResult<CookieStore>
     where
         R: BufRead,
         F: Fn(&str) -> Result<CookieStoreSerialized<'static>, E>,
         crate::Error: From<E>,
     {
-        load_from(reader, cookie_from_str, true)
+        load_from(reader, cookies_from_str, true)
     }
 
     fn load_from<R, E, F>(
         mut reader: R,
-        cookie_from_str: F,
+        cookies_from_str: F,
         include_expired: bool,
     ) -> StoreResult<CookieStore>
     where
@@ -304,21 +304,21 @@ pub mod cookie_store_serialized {
     {
         let mut cookie_store = String::new();
         reader.read_to_string(&mut cookie_store)?;
-        let cookie_store: CookieStoreSerialized = cookie_from_str(&cookie_store)?;
+        let cookie_store: CookieStoreSerialized = cookies_from_str(&cookie_store)?;
         CookieStore::from_cookies(
-            cookie_store.cookies.into_iter().map(|cookie| Ok(cookie)),
+            cookie_store.cookies.into_iter().map(|cookies| Ok(cookies)),
             include_expired,
         )
     }
 
     /// Load JSON-formatted cookies from `reader`, skipping any __expired__ cookies
     pub fn load_json<R: BufRead>(reader: R) -> StoreResult<CookieStore> {
-        load(reader, |cookie| serde_json::from_str(cookie))
+        load(reader, |cookies| serde_json::from_str(cookies))
     }
 
     /// Load JSON-formatted cookies from `reader`, loading both __expired__ and __unexpired__ cookies
     pub fn load_json_all<R: BufRead>(reader: R) -> StoreResult<CookieStore> {
-        load_all(reader, |cookie| serde_json::from_str(cookie))
+        load_all(reader, |cookies| serde_json::from_str(cookies))
     }
 
     /// Serialize any __unexpired__ and __persistent__ cookies in the store with `cookie_to_string`
